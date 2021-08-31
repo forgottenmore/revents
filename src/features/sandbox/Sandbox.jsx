d248976c11bc7e5ae7cd0b0a3d70751b05836980
Jake Klinvex <jakeklinvex@jakes-air.lan>
2021-08-31 11:50:59 -0400

    End of section 8

--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'semantic-ui-react';
 import { openModal } from '../../app/common/modals/modalReducer';
+import TestMap from './TestMap';
+import TestPlaceInput from './TestPlaceInputs';
 import { decrement, increment } from './testReducer';
 
 export default function Sandbox() {
@@ -19,6 +21,10 @@ export default function Sandbox() {
                 } 
                 content='Open Modal' color='teal' 
             />
+            <div style={{marginTop: 15}}>
+                <TestPlaceInput />
+                <TestMap />
+            </div>
         </>
     )
 }
